d86a0d310db04b02b2eb4017ff85eb34692919d1
llipengda <lipengda2@outlook.com>
2023-01-31 11:37:55 +0800

    study_0-1_knapsack Better Solution

--- nowcoder/study_0-1_knapsack.cpp
+++ nowcoder/study_0-1_knapsack.cpp
@@ -10,71 +10,52 @@ HINT 题目描述
 若Vali′<Valmax ，则称第i个蝴蝶结为一个“必选蝴蝶结”。
 清楚姐姐现在获得了调整蝴蝶结好看程度的机会，她想要知道，对于第i个蝴蝶结，在它初始好看程度的基础上，再加上多少，该蝴蝶结就能够成为一个“必选蝴蝶结”。
 HINT 输入描述:
-第一行输入两个正整数N,M(1≤N,M≤100)。
-接下来
-N行，每行输入两个正整数 wi ,vi (1≤wi≤M,vi≤10^9)表示每个蝴蝶结的体积以及好看程度
+第一行输入两个正整数 N,M (1≤N,M≤100(5000 in hard version))。
+接下来N行，每行输入两个正整数 wi ,vi (1≤wi≤M,vi≤10^9)表示每个蝴蝶结的体积以及好看程度
 HINT 输出描述:
 输出N行，每行一个整数，第i行表示在它初始好看程度的基础上，再加上多少，该蝴蝶结就能够成为一个“必选蝴蝶结”。特别的，如果该蝴蝶结已经是一个“必选蝴蝶结”，则输出0。
 */
 #include <bits/stdc++.h>
+#define N 5010
 typedef long long ll;
 using namespace std;
-ll dp[105][105], v[105], b[105], mb[105], res[105];
-ll N, M;
-ll sum(ll i)
+ll dpz[N][N], dpf[N][N], w[N], v[N], maxz[N], maxf[N];
+ll n, m;
+void _dp(ll dp[][N])
 {
-    ll ans = 0;
-    for (int j = 1; j <= N; j++)
-        if (j != i)
-            ans += b[j];
-    return ans;
-}
-void _dp()
-{
-    //~ dp
-    for (int i = 1; i <= N; i++)
-        for (int j = 1; j <= M; j++)
-            if (j >= v[i])
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - v[i]] + b[i]);
+    for (ll i = 1; i <= n; i++)
+        for (ll j = 1; j <= m; j++)
+            if (j - w[i] >= 0)
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
             else
                 dp[i][j] = dp[i - 1][j];
 }
 int main()
 {
-    cin >> N >> M;
-    for (int i = 1; i <= N; i++)
-        cin >> v[i] >> b[i];
-    _dp();
-    mb[0] = dp[N][M];
-    for (int k = 1; k <= N; k++) {
-        v[102] = v[k];
-        b[102] = b[k];
-        v[k] = b[k] = 0;
-        _dp();
-        mb[k] = dp[N][M];
-        v[k] = v[102];
-        b[k] = b[102];
-    }
-    for (int l = 1; l <= N; l++) {
-        if (mb[0] > mb[l])
-            cout << 0 << endl;
-        else {
-            b[103] = b[l];
-            mb[103] = mb[0];
-            //~ 二分查找
-            ll left = b[l], right = sum(l) + 1;
-            while (left < right) {
-                ll mid = left + (right - left) / 2;
-                b[l] = mid;
-                _dp();
-                if (dp[N][M] <= mb[l])
-                    left = mid + 1;
-                else
-                    right = mid;
-            }
-            cout << left - b[103] << endl;
-            b[l] = b[103];
-            mb[0] = mb[103];
+    cin >> n >> m;
+    for (ll i = 1; i <= n; i++)
+        cin >> w[i] >> v[i];
+    _dp(dpz);
+    reverse(w + 1, w + n + 1);
+    reverse(v + 1, v + n + 1);
+    _dp(dpf);
+    reverse(w + 1, w + n + 1);
+    reverse(v + 1, v + n + 1);
+    for (ll i = 1; i <= n; i++) {
+        memset(maxz, 0, sizeof(maxz));
+        memset(maxf, 0, sizeof(maxf));
+        for (ll j = 1; j <= m; j++) {
+            maxz[j] = max(maxz[j - 1], dpz[i - 1][j]);
+            maxf[j] = max(maxf[j - 1], dpf[n - i][j]);
         }
+        ll Vmx = 0, Vmxi = 0;
+        for (ll j = 0; j <= m; j++) {
+            Vmx = max(Vmx, maxz[j] + maxf[m - j]);
+            if (m - j - w[i] >= 0) {
+                Vmxi = max(Vmxi, maxz[j] + maxf[m - j - w[i]] + v[i]);
+                Vmxi = max(Vmxi, maxf[j] + maxz[m - j - w[i]] + v[i]);
+            }
+        }
+        cout << max(0ll, Vmx - Vmxi + 1ll) << endl;
     }
 }
