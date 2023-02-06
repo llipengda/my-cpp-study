66890ac04edf3a459d269d24f890e127eb23be41
llipengda <lipengda2@outlook.com>
2023-02-06 22:07:24 +0800

    modify 0 to nullptr

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define endl '\n'
-#define IO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define IO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
 using namespace std;
 typedef long long ll;
 typedef double db;
