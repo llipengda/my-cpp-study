c2a48b5499709289b3c922096debcf844cb34ee0
llipengda <lipengda2@outlook.com>
2023-02-08 13:25:27 +0800

    add REP&PER in test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 #define endl '\n'
 #define IO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define PER(i, a, b) for (int i = a; i >= b; i--)
 using namespace std;
 typedef long long ll;
 typedef double db;
