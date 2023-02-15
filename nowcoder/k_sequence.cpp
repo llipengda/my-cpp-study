0c314cd7c13b1d1c231e7cf675ee6a3064d7b853
llipengda <lipengda2@outlook.com>
2023-02-15 16:27:42 +0800

    k_sequence test #2 with AC

--- nowcoder/k_sequence.cpp
+++ nowcoder/k_sequence.cpp
@@ -1,5 +1,6 @@
 /*
 来源：https://ac.nowcoder.com/acm/contest/46810/H
+TIME LIMIT: 2s
 HINT 题目描述
 Tokitsukaze 有一个长度为 n 的序列 a，她想把这个序列划分成 k 个 非空子序列 。定义序列的值为这个序列中 只出现一次 的数字的个数。
  对于 k=1 ... n，Tokitsukaze 想知道把序列 a 划分成 k 个 非空子序列 后，所有子序列的值的和最大是多少。
@@ -31,12 +32,18 @@ void solve() {
         cin >> tmp;
         cnt[tmp]++;
     }
+    multiset<int> st;
+    REP(i, 1, N - 1) {
+        st.insert(cnt[i]);
+    }
+    ll lesum = 0, gecnt = st.size();
     REP(k, 1, n) {
-        int ans = 0;
-        REP(j, 1, N) {
-            ans += (cnt[j] > k ? k - 1 : cnt[j]);
+        while (*st.begin() <= k && !st.empty()) {
+            lesum += (ll)*st.begin();
+            st.erase(st.begin());
+            gecnt--;
         }
-        cout << ans << endl;
+        cout << lesum + gecnt * (k - 1) << endl;
     }
 }
 int main() {
