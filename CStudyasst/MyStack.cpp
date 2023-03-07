a1411211ded61af6493a2beaaea83492c169e530
llipengda <lipengda2@outlook.com>
2023-03-07 14:53:46 +0800

    MyStack

---
+++ CStudyasst/MyStack.cpp
@@ -0,0 +1,77 @@
+#include <cstdint>
+#include <iostream>
+#include <stdexcept>
+
+const int maxstack = 100;
+template <typename T>
+class MyStack {
+public:
+    MyStack();
+    void pop();
+    void push(const T& item);
+    int64_t size() const;
+    void clear();
+    bool full() const;
+    bool empty() const;
+    T top() const;
+
+private:
+    T entry[maxstack];
+    int64_t count;
+};
+
+template <typename T>
+MyStack<T>::MyStack() {
+    count = 0;
+    for (int i = 0; i < maxstack; i++) {
+        entry[i] = 0;
+    }
+}
+
+template <typename T>
+void MyStack<T>::pop() {
+    if (count == 0) throw std::underflow_error("underflow");
+    entry[count - 1] = 0;
+    count--;
+}
+
+template <typename T>
+void MyStack<T>::push(const T& item) {
+    if (count >= maxstack) throw std::overflow_error("overflow");
+    entry[count++] = item;
+}
+
+template <typename T>
+T MyStack<T>::top() const {
+    if (count == 0) throw std::underflow_error("underflow");
+    return entry[count - 1];
+}
+
+template <typename T>
+bool MyStack<T>::full() const {
+    if (count == maxstack)
+        return true;
+    else
+        return false;
+}
+
+template <typename T>
+bool MyStack<T>::empty() const {
+    if (count == 0)
+        return true;
+    else
+        return false;
+}
+
+template <typename T>
+void MyStack<T>::clear() {
+    for (int64_t i = 0; i < count; i++) {
+        entry[i] = 0;
+    }
+    count = 0;
+}
+
+template <typename T>
+int64_t MyStack<T>::size() const {
+    return count;
+}
