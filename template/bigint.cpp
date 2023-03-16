ba48f4e5c973e23301c7a4a2004cc77c513235fc
llipengda <lipengda2@outlook.com>
2023-03-16 14:33:09 +0800

    bigint fix bugs in + and -

--- template/bigint.cpp
+++ template/bigint.cpp
@@ -8,10 +8,12 @@ public:
     BigInt(){};
     BigInt(const long long& x) : s(std::to_string(x)) { string_to_vector(); }
     BigInt(const std::string& s) : s(s) { string_to_vector(); }
-    BigInt(const std::vector<int> v, int f = 1) : v(v), f(f) { vector_to_string(); } // HINT v must be reversed
+    BigInt(const std::vector<int> v, int f = 1) : v(v), f(f) { vector_to_string(); }
+    BigInt(const char* s) : s(std::string(s)) { string_to_vector(); }
     BigInt(const BigInt& other) : v(other.v), f(other.f), s(other.s) {}
-    BigInt& operator=(BigInt& other);
-    BigInt& operator=(std::string& other);
+    BigInt& operator=(const BigInt& other);
+    BigInt& operator=(const std::string& other);
+    BigInt& operator=(const char* other);
     BigInt& operator=(long long& other);
     int operator[](const size_t index) const { return v[index]; };
     friend BigInt operator+(const BigInt& lhs, const BigInt& rhs);
@@ -47,6 +49,9 @@ void BigInt::string_to_vector() {
 }
 
 void BigInt::vector_to_string() {
+    while (v.back() == 0) {
+        v.pop_back();
+    }
     std::reverse(v.begin(), v.end());
     s.resize(f == 1 ? v.size() : v.size() + 1);
     if (f == -1) s[0] = '-';
@@ -56,14 +61,20 @@ void BigInt::vector_to_string() {
     std::reverse(v.begin(), v.end());
 }
 
-BigInt& BigInt::operator=(BigInt& other) {
+BigInt& BigInt::operator=(const BigInt& other) {
     v = other.v;
     s = other.s;
     f = other.f;
     return *this;
 }
 
-BigInt& BigInt::operator=(std::string& other) {
+BigInt& BigInt::operator=(const std::string& other) {
+    BigInt n(other);
+    *this = n;
+    return *this;
+}
+
+BigInt& BigInt::operator=(const char* other) {
     BigInt n(other);
     *this = n;
     return *this;
@@ -77,7 +88,7 @@ BigInt& BigInt::operator=(long long& other) {
 
 bool BigInt::operator<(const BigInt& other) const {
     if (f == other.f) {
-        return s < other.s;
+        return f == 1 ? s < other.s : s > other.s;
     } else {
         return f == -1;
     }
@@ -85,9 +96,9 @@ bool BigInt::operator<(const BigInt& other) const {
 
 bool BigInt::operator>(const BigInt& other) const {
     if (f == other.f) {
-        return s > other.s;
+        return f == 1 ? s > other.s : s < other.s;
     } else {
-        return f == -1;
+        return f == 1;
     }
 }
 
@@ -114,11 +125,23 @@ BigInt& BigInt::operator+=(const BigInt& other) {
     for (size_t i = 0; i < v.size(); i++) {
         v[i] += (i < _other.v.size() ? _other.v[i] : 0);
     }
+    size_t negative_pos = 0;
+    bool find_negative_pos = false;
+    for (size_t i = 0; i < v.size(); i++) {
+        if (v[i] <= -10) {
+            v[i] += 10;
+            v[i + 1]--;
+        }
+    }
     for (size_t i = 0; i < v.size() - 1; i++) {
         if (v[i] >= 10) {
             v[i] -= 10;
             v[i + 1]++;
         } else if (v[i] < 0) {
+            if (!find_negative_pos) {
+                negative_pos = i;
+                find_negative_pos = true;
+            }
             v[i] += 10;
             v[i + 1]--;
         }
@@ -126,11 +149,20 @@ BigInt& BigInt::operator+=(const BigInt& other) {
     if (v.back() == -1) {
         v.pop_back();
         f = -1;
-        v[0] -= 10;
-        v[0] = -v[0];
-        for (size_t i = 1; i < v.size(); i++) {
+        v[negative_pos] -= 10;
+        v[negative_pos] = -v[negative_pos];
+        for (size_t i = negative_pos + 1; i < v.size(); i++) {
             v[i] -= 9;
             v[i] = -v[i];
+        }
+        size_t positive_pos = 0;
+        if (negative_pos != 0) {
+            for (; v[positive_pos] == 0; positive_pos++);
+            v[positive_pos] = 10 - v[positive_pos];
+            v[negative_pos]--;
+        }
+        for (size_t i = positive_pos + 1; i < negative_pos; i++) {
+            v[i] = 9 - v[i];
         }
     } else if (v.back() == 0) {
         v.pop_back();
@@ -171,5 +203,6 @@ std::istream& operator>>(std::istream& in, BigInt& a) {
 int main() {
     BigInt a, b;
     std::cin >> a >> b;
-    std::cout << a - b << std::endl;
+    std::cout << (a + b) << std::endl;
+    std::cout << (a - b) << std::endl;
 }
