6796c5b86264c45e509525546499ae93ac5030ec
llipengda <lipengda2@outlook.com>
2023-05-11 00:56:26 +0800

    stack and priority_queue

--- CStudyasst/MyPriorityQueue.cpp
+++ CStudyasst/MyPriorityQueue.cpp
@@ -1,25 +1,26 @@
 #include <algorithm>
 #include <stdexcept>
 
+namespace pdli {
 template <typename T>
-class MyStack {
+class priority_queue {
 public:
     typedef T* iterator;
-    MyStack();
-    MyStack(size_t size);
-    MyStack(const MyStack& other);
-    MyStack& operator=(const MyStack& other);
-    ~MyStack() noexcept;
+    priority_queue();
+    priority_queue(size_t size);
+    priority_queue(const priority_queue& other);
+    priority_queue& operator=(const priority_queue& other);
+    ~priority_queue() noexcept;
     void pop();
     void push(const T& item);
+    void just_push(const T& item);
     void clear();
     bool empty() const;
     iterator begin() const;
     iterator end() const;
     T& top() const;
     size_t size() const;
-    void quick_sort();
-    void heap_sort();
+    void build_heap();
 
 protected:
     T* entry;
@@ -27,39 +28,58 @@ protected:
     size_t _size;
 
 private:
-    void _M_quick_sort(int l, int r);
     void _M_fit();
+    void _M_insert_heap_head(const T& item);
     void _M_build_heap();
     void _M_insert_heap(const T& item, size_t l, size_t r);
 };
 
 template <typename T>
-MyStack<T>::MyStack() {
+priority_queue<T>::priority_queue() {
     count = 0;
     _size = 100;
     entry = new T[_size + 1];
 }
 
 template <typename T>
-MyStack<T>::MyStack(size_t size) {
+priority_queue<T>::priority_queue(size_t size) {
     count = 0;
     _size = size;
     entry = new T[_size + 1];
 }
 
 template <typename T>
-MyStack<T>::~MyStack() noexcept {
+priority_queue<T>::~priority_queue() noexcept {
     delete[] entry;
 }
 
 template <typename T>
-void MyStack<T>::pop() {
+void priority_queue<T>::pop() {
     if (count == 0) throw std::underflow_error("underflow");
+    T _last = entry[count - 1];
+    _M_insert_heap_head(_last);
     count--;
 }
 
 template <typename T>
-void MyStack<T>::push(const T& item) {
+void priority_queue<T>::push(const T& item) {
+    if (count >= _size) {
+        _M_fit();
+    }
+    entry[count] = item;
+    T cur = count;
+    T parent = (cur - 1) / 2;
+    while (cur > 0 && entry[parent] < item) {
+        entry[cur] = entry[parent];
+        cur = parent;
+        parent = (cur - 1) / 2;
+    }
+    entry[cur] = item;
+    ++count;
+}
+
+template <typename T>
+void priority_queue<T>::just_push(const T& item) {
     if (count >= _size) {
         _M_fit();
     }
@@ -67,28 +87,28 @@ void MyStack<T>::push(const T& item) {
 }
 
 template <typename T>
-T& MyStack<T>::top() const {
+T& priority_queue<T>::top() const {
     if (count == 0) throw std::underflow_error("underflow");
-    return entry[count - 1];
+    return entry[0];
 }
 
 template <typename T>
-bool MyStack<T>::empty() const {
+bool priority_queue<T>::empty() const {
     return count == 0;
 }
 
 template <typename T>
-void MyStack<T>::clear() {
+void priority_queue<T>::clear() {
     count = 0;
 }
 
 template <typename T>
-size_t MyStack<T>::size() const {
+size_t priority_queue<T>::size() const {
     return count;
 }
 
 template <typename T>
-void MyStack<T>::_M_fit() {
+void priority_queue<T>::_M_fit() {
     size_t new_size = _size * 2;
     T* new_entry = new T[new_size + 1];
     for (int i = 0; i < _size; i++) {
@@ -100,47 +120,17 @@ void MyStack<T>::_M_fit() {
 }
 
 template <typename T>
-typename MyStack<T>::iterator MyStack<T>::begin() const {
+typename priority_queue<T>::iterator priority_queue<T>::begin() const {
     return &entry[0];
 }
 
 template <typename T>
-typename MyStack<T>::iterator MyStack<T>::end() const {
+typename priority_queue<T>::iterator priority_queue<T>::end() const {
     return &entry[count];
 }
 
 template <typename T>
-void MyStack<T>::_M_quick_sort(int l, int r) {
-    if (l >= r) return;
-    std::swap(entry[l], entry[r - 1]);
-    int pos = l;
-    int pivot = entry[l];
-    for (int i = l + 1; i < r; i++) {
-        if (entry[i] < pivot) {
-            pos++;
-            std::swap(entry[pos], entry[i]);
-        }
-    }
-    std::swap(entry[l], entry[pos]);
-    _M_quick_sort(l, pos);
-    _M_quick_sort(pos + 1, r);
-}
-
-template <typename T>
-void MyStack<T>::quick_sort() {
-    _M_quick_sort(0, count);
-}
-
-template <typename T>
-void MyStack<T>::_M_build_heap() {
-    for (int i = count / 2 - 1; i >= 0; i--) {
-        T cur = entry[i];
-        _M_insert_heap(cur, i, count);
-    }
-}
-
-template <typename T>
-void MyStack<T>::_M_insert_heap(const T& item, size_t l, size_t r) {
+void priority_queue<T>::_M_insert_heap(const T& item, size_t l, size_t r) {
     size_t pos = 2 * l + 1;
     while (pos < r) {
         if (pos < r - 1 && entry[pos] < entry[pos + 1]) {
@@ -158,11 +148,15 @@ void MyStack<T>::_M_insert_heap(const T& item, size_t l, size_t r) {
 }
 
 template <typename T>
-void MyStack<T>::heap_sort() {
-    _M_build_heap();
-    for (int i = count - 1; i > 0; i--) {
-        T tmp = entry[i];
-        entry[i] = entry[0];
-        _M_insert_heap(tmp, 0, i);
+void priority_queue<T>::_M_insert_heap_head(const T& item) {
+    _M_insert_heap(item, 0, count);
+}
+
+template <typename T>
+void priority_queue<T>::build_heap() {
+    for (int i = count / 2 - 1; i >= 0; i--) {
+        T cur = entry[i];
+        _M_insert_heap(cur, i, count);
     }
 }
+} // namespace pdli
