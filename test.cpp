dedf08fd530ed453154fb875590368d3a1a9a06d
llipengda <lipengda2@outlook.com>
2023-07-23 13:38:46 +0800

    small changes

--- test.cpp
+++ test.cpp
@@ -1,12 +1,9 @@
 #include <bits/stdc++.h>
 #define endl '\n'
-#define IO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
-#define REP(i, a, b) for (int i = a; i <= b; i++)
-#define PER(i, a, b) for (int i = a; i >= b; i--)
-using namespace std;
+#define IO std::ios::sync_with_stdio(false), std::cin.tie(nullptr)
 using ll = long long;
-using db = double;
-using pii = pair<int, int>;
+using pii = std::pair<int, int>;
+using namespace std;
 int main() {
     
 }
