e4f8e537485979a3a12e7032f799b8121a314708
llipengda <lipengda2@outlook.com>
2024-02-04 22:15:17 +0800

    modify bigint template

--- template/bigint.cpp
+++ template/bigint.cpp
@@ -18,43 +18,45 @@ public:
     std::vector<int> multi(std::vector<int>& AA, std::vector<int>& BB);
 };
 
-class BigInt {
+class big_int {
 public:
     static bool using_fft;
-    BigInt(){};
-    BigInt(const long long& x) : s(std::to_string(x)) { string_to_vector(); }
-    BigInt(const std::string& s) : s(s) { string_to_vector(); }
-    BigInt(const std::vector<int> v, int f = 1) : v(v), f(f) { vector_to_string(); }
-    BigInt(const char* s) : s(std::string(s)) { string_to_vector(); }
-    BigInt(const BigInt& other) : v(other.v), f(other.f), s(other.s) {}
-    BigInt& operator=(const BigInt& other);
-    BigInt& operator=(const std::string& other);
-    BigInt& operator=(const char* other);
-    BigInt& operator=(long long& other);
-    friend BigInt operator+(const BigInt& lhs, const BigInt& rhs);
-    friend BigInt operator-(const BigInt& lhs, const BigInt& rhs);
-    friend BigInt operator*(const BigInt& lhs, const BigInt& rhs);
-    friend BigInt operator*(const BigInt& lhs, const long long& rhs);
-    friend BigInt operator/(const BigInt& lhs, const BigInt& rhs);
-    friend BigInt operator/(const BigInt& lhs, const long long& rhs);
-    friend BigInt operator%(const BigInt& lhs, const BigInt& rhs);
-    friend BigInt operator%(const BigInt& lhs, const long long& rhs);
-    BigInt& operator+=(const BigInt& other);
-    BigInt& operator-=(const BigInt& other);
-    BigInt& operator*=(const BigInt& other);
-    BigInt& operator*=(const long long& other);
-    BigInt& operator/=(const BigInt& other);
-    BigInt& operator/=(const long long& other);
-    BigInt& operator%=(const BigInt& other);
-    BigInt& operator%=(const long long& other);
-    bool operator<(const BigInt& other) const;
-    bool operator>(const BigInt& other) const;
-    bool operator<=(const BigInt& other) const;
-    bool operator>=(const BigInt& other) const;
-    bool operator==(const BigInt& other) const;
-    bool operator!=(const BigInt& other) const;
-    friend std::ostream& operator<<(std::ostream& out, const BigInt& a);
-    friend std::istream& operator>>(std::istream& in, BigInt& a);
+    big_int(){};
+    big_int(const long long& x) : s(std::to_string(x)) { string_to_vector(); }
+    big_int(const std::string& s) : s(s) { string_to_vector(); }
+    big_int(const std::vector<int> v, int f = 1) : v(v), f(f) { vector_to_string(); }
+    big_int(const char* s) : s(std::string(s)) { string_to_vector(); }
+    big_int(const big_int& other) : v(other.v), s(other.s), f(other.f) {}
+    big_int(big_int&& other) : v(std::move(other.v)), s(std::move(other.s)), f(other.f) {}
+    big_int& operator=(const big_int& other);
+    big_int& operator=(big_int&& other);
+    big_int& operator=(const std::string& other);
+    big_int& operator=(const char* other);
+    big_int& operator=(long long& other);
+    friend big_int operator+(const big_int& lhs, const big_int& rhs);
+    friend big_int operator-(const big_int& lhs, const big_int& rhs);
+    friend big_int operator*(const big_int& lhs, const big_int& rhs);
+    friend big_int operator*(const big_int& lhs, const long long& rhs);
+    friend big_int operator/(const big_int& lhs, const big_int& rhs);
+    friend big_int operator/(const big_int& lhs, const long long& rhs);
+    friend big_int operator%(const big_int& lhs, const big_int& rhs);
+    friend big_int operator%(const big_int& lhs, const long long& rhs);
+    big_int& operator+=(const big_int& other);
+    big_int& operator-=(const big_int& other);
+    big_int& operator*=(const big_int& other);
+    big_int& operator*=(const long long& other);
+    big_int& operator/=(const big_int& other);
+    big_int& operator/=(const long long& other);
+    big_int& operator%=(const big_int& other);
+    big_int& operator%=(const long long& other);
+    bool operator<(const big_int& other) const;
+    bool operator>(const big_int& other) const;
+    bool operator<=(const big_int& other) const;
+    bool operator>=(const big_int& other) const;
+    bool operator==(const big_int& other) const;
+    bool operator!=(const big_int& other) const;
+    friend std::ostream& operator<<(std::ostream& out, const big_int& a);
+    friend std::istream& operator>>(std::istream& in, big_int& a);
     size_t size() const;
     bool iszero() const;
 
@@ -73,7 +75,9 @@ void FFT::expand(int temp) {
     for (_N = 1; _N <= temp; _N <<= 1, L++)
         ;
     r.resize(_N + 1);
-    for (int i = 0; i < _N; ++i) r[i] = r[i >> 1] >> 1 | (i & 1) << (L - 1);
+    for (int i = 0; i < _N; ++i) {
+        r[i] = r[i >> 1] >> 1 | (i & 1) << (L - 1);
+    }
     return;
 }
 
@@ -93,7 +97,9 @@ void FFT::fft(std::vector<std::complex<double>>& A, int f) {
     }
 }
 
-std::vector<int> FFT::mul(std::vector<std::complex<double>>& A, std::vector<std::complex<double>>& B) {
+std::vector<int>
+FFT::mul(std::vector<std::complex<double>>& A,
+         std::vector<std::complex<double>>& B) {
     expand(_n + _m);
     A.resize(_N + 1);
     B.resize(_N + 1);
@@ -107,7 +113,8 @@ std::vector<int> FFT::mul(std::vector<std::complex<double>>& A, std::vector<std:
     return vec;
 }
 
-std::vector<int> FFT::multi(std::vector<int>& AA, std::vector<int>& BB) {
+std::vector<int>
+FFT::multi(std::vector<int>& AA, std::vector<int>& BB) {
     std::vector<std::complex<double>> A, B;
     _n = AA.size();
     _m = BB.size();
@@ -118,7 +125,7 @@ std::vector<int> FFT::multi(std::vector<int>& AA, std::vector<int>& BB) {
     return mul(A, B);
 }
 
-void BigInt::string_to_vector() {
+void big_int::string_to_vector() {
     if (s[0] == '-') f = -1;
     v.resize(f == 1 ? s.size() : s.size() - 1);
     for (size_t i = (f == 1 ? 0 : 1); i < (f == 1 ? v.size() : v.size() + 1); i++) {
@@ -127,7 +134,7 @@ void BigInt::string_to_vector() {
     std::reverse(v.begin(), v.end());
 }
 
-void BigInt::vector_to_string() {
+void big_int::vector_to_string() {
     while (v.back() == 0 && v.size() > 1) v.pop_back();
     std::reverse(v.begin(), v.end());
     s.resize(f == 1 ? v.size() : v.size() + 1);
@@ -151,32 +158,39 @@ bool greater_eq(std::vector<int> a, std::vector<int> b, int last_dg, int len) {
     return true;
 }
 
-BigInt& BigInt::operator=(const BigInt& other) {
+big_int& big_int::operator=(const big_int& other) {
     v = other.v;
     s = other.s;
     f = other.f;
     return *this;
 }
 
-BigInt& BigInt::operator=(const std::string& other) {
-    BigInt n(other);
+big_int& big_int::operator=(big_int&& other) {
+    v = std::move(other.v);
+    s = std::move(other.s);
+    f = other.f;
+    return *this;
+}
+
+big_int& big_int::operator=(const std::string& other) {
+    big_int n(other);
     *this = n;
     return *this;
 }
 
-BigInt& BigInt::operator=(const char* other) {
-    BigInt n(other);
+big_int& big_int::operator=(const char* other) {
+    big_int n(other);
     *this = n;
     return *this;
 }
 
-BigInt& BigInt::operator=(long long& other) {
-    BigInt n(other);
+big_int& big_int::operator=(long long& other) {
+    big_int n(other);
     *this = n;
     return *this;
 }
 
-bool BigInt::operator<(const BigInt& other) const {
+bool big_int::operator<(const big_int& other) const {
     if (f == other.f) {
         return f == 1 ? s < other.s : s > other.s;
     } else {
@@ -184,7 +198,7 @@ bool BigInt::operator<(const BigInt& other) const {
     }
 }
 
-bool BigInt::operator>(const BigInt& other) const {
+bool big_int::operator>(const big_int& other) const {
     if (f == other.f) {
         return f == 1 ? s > other.s : s < other.s;
     } else {
@@ -192,16 +206,16 @@ bool BigInt::operator>(const BigInt& other) const {
     }
 }
 
-bool BigInt::operator<=(const BigInt& other) const { return !(*this > other); }
+bool big_int::operator<=(const big_int& other) const { return !(*this > other); }
 
-bool BigInt::operator>=(const BigInt& other) const { return !(*this < other); }
+bool big_int::operator>=(const big_int& other) const { return !(*this < other); }
 
-bool BigInt::operator==(const BigInt& other) const { return !((*this < other) && (*this > other)); }
+bool big_int::operator==(const big_int& other) const { return !((*this < other) && (*this > other)); }
 
-bool BigInt::operator!=(const BigInt& other) const { return !(*this == other); }
+bool big_int::operator!=(const big_int& other) const { return !(*this == other); }
 
-BigInt& BigInt::operator+=(const BigInt& other) {
-    BigInt _other(other);
+big_int& big_int::operator+=(const big_int& other) {
+    big_int _other(other);
     v.resize(std::max(v.size(), _other.v.size()) + 1);
     v.back() = 0;
     if (f == -1) {
@@ -238,8 +252,11 @@ BigInt& BigInt::operator+=(const BigInt& other) {
         f = 1;
     }
     if (v.back() < 0) {
-        if (v.back() == -1) v.pop_back();
-        else v.back() += 10;
+        if (v.back() == -1) {
+            v.pop_back();
+        } else {
+            v.back() += 10;
+        }
         f = -1;
         v[negative_pos] -= 10;
         v[negative_pos] = -v[negative_pos];
@@ -266,61 +283,61 @@ BigInt& BigInt::operator+=(const BigInt& other) {
     return *this;
 }
 
-BigInt& BigInt::operator-=(const BigInt& other) {
-    BigInt _other(other);
+big_int& big_int::operator-=(const big_int& other) {
+    big_int _other(other);
     _other.f = -_other.f;
     *this += _other;
     return *this;
 }
 
-BigInt& BigInt::operator*=(const BigInt& other) {
+big_int& big_int::operator*=(const big_int& other) {
     *this = *this * other;
     return *this;
 }
 
-BigInt& BigInt::operator*=(const long long& other) {
+big_int& big_int::operator*=(const long long& other) {
     *this = *this * other;
     return *this;
 }
 
-BigInt& BigInt::operator/=(const BigInt& other) {
+big_int& big_int::operator/=(const big_int& other) {
     *this = *this / other;
     return *this;
 }
 
-BigInt& BigInt::operator/=(const long long& other) {
+big_int& big_int::operator/=(const long long& other) {
     *this = *this / other;
     return *this;
 }
 
-BigInt& BigInt::operator%=(const BigInt& other) {
+big_int& big_int::operator%=(const big_int& other) {
     *this = *this % other;
     return *this;
 }
 
-BigInt& BigInt::operator%=(const long long& other) {
+big_int& big_int::operator%=(const long long& other) {
     *this = *this % other;
     return *this;
 }
 
-BigInt operator+(const BigInt& lhs, const BigInt& rhs) {
-    BigInt res(lhs);
+big_int operator+(const big_int& lhs, const big_int& rhs) {
+    big_int res(lhs);
     return res += rhs;
 }
 
-BigInt operator-(const BigInt& lhs, const BigInt& rhs) {
-    BigInt res(lhs);
+big_int operator-(const big_int& lhs, const big_int& rhs) {
+    big_int res(lhs);
     return res -= rhs;
 }
 
-BigInt operator*(const BigInt& lhs, const BigInt& rhs) {
-    BigInt res;
+big_int operator*(const big_int& lhs, const big_int& rhs) {
+    big_int res;
     if (lhs.f == rhs.f) {
         res.f = 1;
     } else {
         res.f = -1;
     }
-    if (BigInt::using_fft) {
+    if (big_int::using_fft) {
         FFT F;
         std::vector<int> _lhs = lhs.v;
         std::vector<int> _rhs = rhs.v;
@@ -334,7 +351,7 @@ BigInt operator*(const BigInt& lhs, const BigInt& rhs) {
         return res;
     }
     if (lhs.s == "0" || rhs.s == "0") {
-        return BigInt(0LL);
+        return big_int(0LL);
     }
     res.v.resize(lhs.v.size() + rhs.v.size());
     for (auto& i : res.v) i = 0;
@@ -351,14 +368,14 @@ BigInt operator*(const BigInt& lhs, const BigInt& rhs) {
     return res;
 }
 
-BigInt operator*(const BigInt& lhs, const long long& rhs) {
+big_int operator*(const big_int& lhs, const long long& rhs) {
     if (lhs.s == "0" || rhs == 0) {
-        return BigInt(0LL);
+        return big_int(0LL);
     }
-    if (rhs > 9223372036854775807LL / 9 || BigInt::using_fft) {
-        return lhs * BigInt(rhs);
+    if (rhs > 9223372036854775807LL / 9 || big_int::using_fft) {
+        return lhs * big_int(rhs);
     }
-    BigInt res;
+    big_int res;
     if (lhs.f == (rhs > 0 ? 1 : (rhs < 0 ? -1 : 0))) {
         res.f = 1;
     } else {
@@ -381,15 +398,15 @@ BigInt operator*(const BigInt& lhs, const long long& rhs) {
     return res;
 }
 
-BigInt operator/(const BigInt& lhs, const BigInt& rhs) {
-    BigInt res;
+big_int operator/(const big_int& lhs, const big_int& rhs) {
+    big_int res;
     if (lhs.f == rhs.f) {
         res.f = 1;
     } else {
         res.f = -1;
     }
     if (rhs.s == "0") throw std::runtime_error("division by zero");
-    BigInt rem = lhs;
+    big_int rem = lhs;
     rem.v.resize(lhs.v.size() + rhs.v.size());
     res.v.resize(lhs.v.size() + rhs.v.size());
     for (int i = lhs.v.size() - rhs.v.size(); i >= 0; i--) {
@@ -408,19 +425,19 @@ BigInt operator/(const BigInt& lhs, const BigInt& rhs) {
     return res;
 }
 
-BigInt operator/(const BigInt& lhs, const long long& rhs) {
-    return lhs / BigInt(rhs);
+big_int operator/(const big_int& lhs, const long long& rhs) {
+    return lhs / big_int(rhs);
 }
 
-BigInt operator%(const BigInt& lhs, const BigInt& rhs) {
-    BigInt res;
+big_int operator%(const big_int& lhs, const big_int& rhs) {
+    big_int res;
     if (lhs.f == rhs.f) {
         res.f = 1;
     } else {
         res.f = -1;
     }
     if (rhs.s == "0") throw std::runtime_error("division by zero");
-    BigInt rem = lhs;
+    big_int rem = lhs;
     rem.v.resize(lhs.v.size() + rhs.v.size());
     res.v.resize(lhs.v.size() + rhs.v.size());
     for (int i = lhs.v.size() - rhs.v.size(); i >= 0; i--) {
@@ -439,34 +456,34 @@ BigInt operator%(const BigInt& lhs, const BigInt& rhs) {
     return rem;
 }
 
-BigInt operator%(const BigInt& lhs, const long long& rhs) {
-    return lhs / BigInt(rhs);
+big_int operator%(const big_int& lhs, const long long& rhs) {
+    return lhs / big_int(rhs);
 }
 
-std::ostream& operator<<(std::ostream& out, const BigInt& a) {
+std::ostream& operator<<(std::ostream& out, const big_int& a) {
     out << a.s;
     return out;
 }
 
-std::istream& operator>>(std::istream& in, BigInt& a) {
-    std::string str;
+std::istream& operator>>(std::istream& in, big_int& a) {
+    std::string str{};
     in >> str;
     a = str;
     return in;
 }
 
-size_t BigInt::size() const {
+size_t big_int::size() const {
     return v.size();
 }
 
-bool BigInt::iszero() const {
+bool big_int::iszero() const {
     return s == "0";
 }
 
-bool BigInt::using_fft = false;
+bool big_int::using_fft = false;
 
 int main() {
-    BigInt a, b;
+    big_int a, b;
     std::cin >> a >> b;
     std::cout << a + b << std::endl
               << a - b << std::endl
